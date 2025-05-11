81c8fda6f848c35556002282a38a1d1a6de7c7b2
Shailendra Chauhan <pro.shailendra@gmail.com>
2025-05-11 17:08:52 +0530

    added things

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp11May.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
