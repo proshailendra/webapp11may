f1705e62bb70d9d49464067aa040b24cefce61f8
Shailendra Chauhan <pro.shailendra@gmail.com>
2025-05-18 15:54:16 +0530

    added DAL layer

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp11May.Controllers
+{
+    public class ProductController : Controller
+    {
+        AppDbContext _db;
+        public ProductController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var products = _db.Products.ToList();
+            return View(products);
+        }
+    }
+}
